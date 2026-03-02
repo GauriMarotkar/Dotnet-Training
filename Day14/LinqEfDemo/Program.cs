d9637206b34ca2cb30bd4af123356d8a2690e66d
GauriMarotkar <gaurimarotkar@gmail.com>
2026-03-02 12:46:45 +0530

    Initial commit for Day14 LinqEfDemo project

---
+++ Day14/LinqEfDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+var _context = new CrmContext();
+var customers = _context.Customers.ToList();
+
+public class CrmContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=CrmDB;Trusted_Connection=True;TrustServerCertificate=True;");
+    }
+}
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+public class Order
+{
+    public int Id { get; set; }
+    public decimal Amount { get; set; }
+    public int CustomerId { get; set; }
+    public Customer Customer { get; set; }
+}
